eb606bbda5e3323c20c76d579eeb6ea67d23e5e7
ali <alixanxoldorov@gmail.com>
2021-06-21 14:34:36 +0000

    ali

--- ustoz/settings.py
+++ ustoz/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import whitenoise
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ustoz/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
